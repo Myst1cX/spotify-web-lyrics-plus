55187ba1e2e6d1471b9d000c32a7604c7cd27547
Myst1cX <97919309+Myst1cX@users.noreply.github.com>
2026-04-03 22:00:03 +0200

    Create pip.ts

---
+++ pip.ts
@@ -0,0 +1,53 @@
+// https://w3c.github.io/picture-in-picture
+// https://bugzilla.mozilla.org/show_bug.cgi?id=pip
+import { configPromise, localConfig } from './config';
+import { appendStyle, querySelector, isSafari } from './utils';
+import { optionsPromise } from './options';
+import { lyricVideo } from './element';
+
+const nativeExitPictureInPicture = document.exitPictureInPicture?.bind(document);
+
+if (document.exitPictureInPicture) {
+  // Only allow extensions to exit lyrics pip
+  document.exitPictureInPicture = async function () {
+    if (document.pictureInPictureElement !== lyricVideo) {
+      await nativeExitPictureInPicture();
+    }
+  };
+}
+
+export const openLyrics = async function () {
+  const options = await optionsPromise;
+  if (options['show-on'] === 'pip' && document.pictureInPictureEnabled) {
+    if (isSafari) {
+      const showLitter = 'position:absolute;left:calc(100% - 1px);bottom:calc(100% - 1px)';
+      lyricVideo.setAttribute('style', showLitter);
+      document.body.append(lyricVideo);
+    }
+    return await lyricVideo.requestPictureInPicture();
+  }
+
+  const { LYRICS_CONTAINER_SELECTOR, PAGE_PIP_STYLE } = await configPromise;
+  const container = querySelector(LYRICS_CONTAINER_SELECTOR);
+  if (container) {
+    lyricVideo.setAttribute('style', PAGE_PIP_STYLE);
+    container.append(lyricVideo);
+    lyricVideo.dispatchEvent(new CustomEvent('enterpictureinpicture'));
+    return;
+  } else {
+    throw new Error('Page PiP container not found');
+  }
+};
+
+export const closeLyrics = async function () {
+  if (document.pictureInPictureElement === lyricVideo) {
+    return await nativeExitPictureInPicture();
+  }
+
+  lyricVideo.dispatchEvent(new CustomEvent('leavepictureinpicture'));
+};
+
+// https://github.com/mantou132/Spotify-Lyrics/issues/31
+if (!document.pictureInPictureEnabled) {
+  appendStyle(localConfig.NO_PIP_STYLE);
+}
